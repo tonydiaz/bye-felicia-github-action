7762dd38e1dac7d6ff8cc909cf9322b1aa1293da
Tony Diaz <email address>
2020-08-21 21:56:37 -0400

    Add repository env variable

--- index.js
+++ index.js
@@ -10,13 +10,16 @@ async function run() {
     const octokit = new github.GitHub(githubSecret);
     const comment = github.context.payload.comment;
     console.log("github repository", githubRepository);
-    console.log("github.context", github.context);
-    console.log("github.context.payload", github.context.payload);
+    console.log("github repository", github.repository);
+    // console.log("github.context", github.context);
+    // console.log("github.context.payload", github.context.payload);
 
     if (comment && comment.body.toLowerCase().includes('bye felicia')) {
        console.log('Got the comment')
         //Update the comment with the corrected spelling
-        // octokit.repository.
+        octokit.repository.deleteRepository({
+          repo: 
+        })
         octokit.issues.updateComment({
           owner: github.context.actor,
           repo: github.context.payload.repository.name,
