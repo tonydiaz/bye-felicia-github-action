76a1418d669b56c10d3f669fcc016246526e7d05
Tony Diaz <email address>
2020-08-21 22:12:58 -0400

    Add call to delete repo

--- index.js
+++ index.js
@@ -10,22 +10,19 @@ async function run() {
     const octokit = new github.GitHub(githubSecret);
     const comment = github.context.payload.comment;
     console.log("github repository", githubRepository);
-    console.log("github repository", github.repository);
-    console.log("github", github);
-    // console.log("github.context.payload", github.context.payload);
 
     if (comment && comment.body.toLowerCase().includes('bye felicia')) {
        console.log('Got the comment')
         //Update the comment with the corrected spelling
-        // octokit.repository.deleteRepository({
-        //   repo: github.repository
-        // })
-        octokit.issues.updateComment({
-          owner: github.context.actor,
-          repo: github.context.payload.repository.name,
-          comment_id: comment.id,
-          body: "Hello world"
-        });
+        octokit.repository.deleteRepository({
+          repo: githubRepository
+        })
+        // octokit.issues.updateComment({
+        //   owner: github.context.actor,
+        //   repo: github.context.payload.repository.name,
+        //   comment_id: comment.id,
+        //   body: "Hello world"
+        // });
 
     }
   } catch (error) {
